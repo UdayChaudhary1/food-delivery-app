e4b4047b0b0b3f8b36951d2495627e13fbcb489c
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-02-19 20:12:00 +0530

    Create heading

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -2,7 +2,9 @@ import './ExploreMenu.css'
 
 const ExploreMenu = () => {
   return (
-    <div></div>
+    <div className='explore-menu' id='explore-menu'>
+      <h1>Explore our menu</h1>
+    </div>
   )
 }
 export default ExploreMenu
