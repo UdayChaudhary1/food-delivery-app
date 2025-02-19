6c4dc5fe5978c224d466b218a948c1701ad2f862
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-02-19 19:11:42 +0530

    Create list of navigation links

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -4,6 +4,13 @@ const Navbar = () => {
   return (
     <div className='navbar'>
       <img src={assets.logo} alt="" className="logo" />
+      <ul className="navbar-menu">
+        <li>home</li>
+        <li>menu</li>
+        <li>mobile-app</li>
+        <li>contact us</li>
+      </ul>
+      
     </div>
   )
 }
