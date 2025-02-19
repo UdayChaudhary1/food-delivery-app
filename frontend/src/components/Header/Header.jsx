8ef6cf604afb62d0ed5ad5ff56b92976bd28ec2b
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-02-19 19:56:56 +0530

    Add button to view menu

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,12 +1,17 @@
-import './Header.css'
+import "./Header.css";
 
 const Header = () => {
   return (
     <div className="header">
       <div className="header-contents">
         <h2>Order your favourite food here</h2>
+        <p>
+          Choose from a diverse menu of delicious dishes made with fresh
+          ingredients and carefully prepared by our talented chefs.
+        </p>
+        <button>View Menu</button>
       </div>
     </div>
-  )
-}
-export default Header
+  );
+};
+export default Header;
