b9f258c88775cc8e13e5ce041d98f47f317db3be
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-02-19 20:17:05 +0530

    Display image and item name from menu
    Iterate over menu_list to display image and item
    name.

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -9,6 +9,16 @@ const ExploreMenu = () => {
         Choose from a diverse menu of delicious dishes made with fresh
         ingredients and carefully prepared by our talented chefs.
       </p>
+      <div className="explore-menu-list">
+        {menu_list.map((item, index) => {
+          return (
+            <div key={index} className="explore-menu-list-item">
+              <img src={item.menu_image} alt="" />
+              <p>{item.menu_name}</p>
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
