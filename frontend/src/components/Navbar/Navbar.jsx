0cb446a8b8676686709598e8375bb0202f69b75f
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-02-19 19:10:23 +0530

    Add logo

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,10 @@
+import { assets } from '../../assets/assets'
 import './Navbar.css'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <img src={assets.logo} alt="" className="logo" />
+    </div>
   )
 }
 export default Navbar
