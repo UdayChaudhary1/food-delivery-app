008af1803241462cf2000cf7d68a543cbbbfc29a
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-02-19 19:56:02 +0530

    Add heading

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -2,7 +2,11 @@ import './Header.css'
 
 const Header = () => {
   return (
-    <div className="header"></div>
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+      </div>
+    </div>
   )
 }
 export default Header
