eab6e267d4e6ead0ece9bfeeac81d8508c0080ed
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-02-19 19:32:12 +0530

    Create menu variable to store state of links
    Add classes to the links to determine whether
    the link is active or not using menu variable.

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,16 @@
 import { assets } from '../../assets/assets'
 import './Navbar.css'
 const Navbar = () => {
+
+  const [menu, setMenu] = useState("home")
   return (
     <div className='navbar'>
       <img src={assets.logo} alt="" className="logo" />
       <ul className="navbar-menu">
-        <li>home</li>
-        <li>menu</li>
-        <li>mobile-app</li>
-        <li>contact us</li>
+        <li className={menu=="home"?"active":""}>home</li>
+        <li className={menu=="menu"?"active":""}>menu</li>
+        <li className={menu=="mobile-app"?"active":""}>mobile-app</li>
+        <li className={menu=="contact-us"?"active":""}>contact us</li>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
