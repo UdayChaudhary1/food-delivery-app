b1eabf9caebdf003a8be76d936ea4dfeb9d998fb
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-02-21 09:21:17 +0530

    Add food_list to context value

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,9 +1,11 @@
 import { createContext } from "react";
-
+import {food_list} from '../assets/assets.js'
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
-  const contextValue = {};
+  const contextValue = {
+    food_list
+  };
   return (
     <StoreContext.Provider value={contextValue}>
       {props.children}
