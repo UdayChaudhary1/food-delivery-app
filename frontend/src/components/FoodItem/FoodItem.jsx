c3a8a9b8283b5a4c99d4c67c1011f11f96e57a88
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-02-21 09:50:47 +0530

    Add food item image

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -2,7 +2,11 @@ import './FoodItem.css'
 
 const FoodItem = ({id, name , price, description, image}) => {
   return (
-    <div>FoodItem</div>
+    <div className='food-item'>
+      <div className='food-item-img-container'>
+        <img className='food-item-image' src={image} alt="" />
+      </div>
+    </div>
   )
 }
 export default FoodItem
