0b0760236ab679f7d9fa13d853a566e8d1906d1b
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-02-21 09:53:59 +0530

    Add price for food items

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,21 +1,21 @@
-import { assets } from '../../assets/assets'
-import './FoodItem.css'
+import { assets } from "../../assets/assets";
+import "./FoodItem.css";
 
-const FoodItem = ({id, name , price, description, image}) => {
+const FoodItem = ({ id, name, price, description, image }) => {
   return (
-    <div className='food-item'>
-      <div className='food-item-img-container'>
-        <img className='food-item-image' src={image} alt="" />
+    <div className="food-item">
+      <div className="food-item-img-container">
+        <img className="food-item-image" src={image} alt="" />
       </div>
       <div className="foodt-item-info">
         <div className="food-item-name-rating">
           <p>{name}</p>
           <img src={assets.rating_starts} alt="" />
         </div>
-        <p className='food-item-desc'>{description}</p>
-        
+        <p className="food-item-desc">{description}</p>
+        <p className="food-item-price">{price}</p>
       </div>
     </div>
-  )
-}
-export default FoodItem
+  );
+};
+export default FoodItem;
