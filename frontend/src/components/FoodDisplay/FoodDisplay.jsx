6fbd5af4e11f583f2c0348acbce9a68b6c09a630
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-02-21 09:28:20 +0530

    Destructure category

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import "./FoodDisplay.css";
 import { StoreContext } from "../../context/StoreContext";
-const FoodDisplay = () => {
+const FoodDisplay = ({category}) => {
 
   const { food_list } = useContext(StoreContext);
 
