6c1cbb282f88b6629924bc66860cbf06b1b0bdf0
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-02-23 10:25:36 +0530

    Remove useState import

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,6 +1,7 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { assets } from "../../assets/assets";
 import "./FoodItem.css";
+import { StoreContext } from "../../context/StoreContext";
 
 const FoodItem = ({ id, name, price, description, image }) => {
   const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
