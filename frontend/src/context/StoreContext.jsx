196d25bb2cf107ab882b10b36292a73646058eba
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-02-23 09:17:41 +0530

    Create the functionality of storing items in cart

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,6 +5,13 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
 
+  const addToCart = (itemId) => {
+    if (!cartItems[itemId]) {
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+    } else {
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    }
+  };
 
   const contextValue = {
     food_list,
