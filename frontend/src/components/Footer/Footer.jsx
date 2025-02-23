75f8e63f8c537dd094d3ac014a7cb086026bc4cc
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-02-23 09:52:34 +0530

    Add social icons to the footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,18 +1,27 @@
-import { assets } from '../../assets/assets'
-import './Footer.css'
+import { assets } from "../../assets/assets";
+import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div className="footer" id='footer'>
+    <div className="footer" id="footer">
       <div className="footer-content">
         <div className="footer-content-left">
           <img src={assets.logo} alt="" />
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati placeat ipsum voluptatem aperiam minima autem voluptate fuga incidunt alias maiores, totam cumque porro! Vel, porro!</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati
+            placeat ipsum voluptatem aperiam minima autem voluptate fuga
+            incidunt alias maiores, totam cumque porro! Vel, porro!
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
         </div>
         <div className="footer-content-center"></div>
         <div className="footer-content-right"></div>
       </div>
     </div>
-  )
-}
-export default Footer
+  );
+};
+export default Footer;
