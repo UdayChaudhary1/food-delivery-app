170e22862baeb70892e9991880bc3441b7645cf8
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-02-23 09:51:21 +0530

    Add logo to the left of footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,8 +1,18 @@
+import { assets } from '../../assets/assets'
 import './Footer.css'
 
 const Footer = () => {
   return (
-    <div className="footer">Footer</div>
+    <div className="footer" id='footer'>
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="" />
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati placeat ipsum voluptatem aperiam minima autem voluptate fuga incidunt alias maiores, totam cumque porro! Vel, porro!</p>
+        </div>
+        <div className="footer-content-center"></div>
+        <div className="footer-content-right"></div>
+      </div>
+    </div>
   )
 }
 export default Footer
