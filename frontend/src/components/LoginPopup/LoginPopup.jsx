d4b955f3644bd904c3136dc3e5ebe2d3febd8850
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-02-25 09:12:11 +0530

    Display cross icon to close the login popup

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -10,6 +10,7 @@ const LoginPopup = ({setShowLogin}) => {
     <form className="login-popup-container">
       <div className="login-popup-title">
         <h2>{currState}</h2>
+        <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
       </div>
     </form>
   </div>;
