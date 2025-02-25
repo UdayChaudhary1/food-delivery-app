55bd0e83c5e59771c101806b664dd9e9a224fd2a
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-02-25 09:10:59 +0530

    Display heading of current login or sign up state

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react';
 import './LoginPopup.css'
+import { assets } from '../../assets/assets';
 
 const LoginPopup = ({setShowLogin}) => {
 
   const [currState, setCurrState] = useState("Sign Up")
-  
+
   return <div className="Login-popup">
     <form className="login-popup-container">
       <div className="login-popup-title">
+        <h2>{currState}</h2>
       </div>
     </form>
   </div>;
