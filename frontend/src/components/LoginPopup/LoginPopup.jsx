e07998e2f963dd8f1a9f7eb7c7fdba9bb7c3ad03
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-02-25 09:14:17 +0530

    Display input field for name, email and password

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -12,6 +12,11 @@ const LoginPopup = ({setShowLogin}) => {
         <h2>{currState}</h2>
         <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
       </div>
+      <div className="login-popup-inputs">
+        <input type="text" placeholder='Your name' required />
+        <input type="email" placeholder='Your email' required />
+        <input type="password" placeholder='Password' required />
+      </div>
     </form>
   </div>;
 }
