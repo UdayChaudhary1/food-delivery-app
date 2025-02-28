1622faf8f90e5931d04a9ce702a6efdda9bfed18
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-02-28 15:13:22 +0530

    Add image storage engine

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,8 +1,16 @@
 import express from "express";
-import { addFood } from "../controllers/foodControllers.js";
+import { addFood } from "../controllers/foodController.js";
 import multer from "multer";
 
 const foodRouter = express.Router();
+
+//Image storage engine
+const storage = multer.diskStorage({
+  destination: "uploads",
+  filename: (req, file, cb) => {
+    return cb(null, `${Date.now()}${file.originalname}`);
+  },
+});
 
 foodRouter.post("/add", addFood);
 
