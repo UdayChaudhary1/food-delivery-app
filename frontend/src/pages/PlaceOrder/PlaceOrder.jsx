7bbd25cca56c8cf072f754c8414466f6d7c217df
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-02-28 09:28:21 +0530

    Display the cart total section

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,6 +1,10 @@
+import { useContext } from "react";
 import "./PlaceOrder.css";
+import { StoreContext } from "../../context/StoreContext";
 
 const PlaceOrder = () => {
+  const { getTotalCartAmount } = useContext(StoreContext);
+
   return (
     <form className="place-order">
       <div className="place-order-left">
@@ -21,7 +25,28 @@ const PlaceOrder = () => {
         </div>
         <input type="text" placeholder="Phone" />
       </div>
-      <div className="place-order-right"></div>
+      <div className="place-order-right">
+        <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>${2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>${getTotalCartAmount() + 2}</b>
+            </div>
+          </div>
+          <button>PROCEED TO PAYMENT</button>
+        </div>
+      </div>
     </form>
   );
 };
