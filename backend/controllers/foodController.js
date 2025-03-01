ed10c643856f5deeaf10a4fd67256052dd6e6d89
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-03-01 09:22:11 +0530

    add logic to add food to uploads

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -2,8 +2,23 @@ import foodModel from "../models/foodModel.js";
 import fs from "fs";
 
 //add food item
-const addFoodItem = async (req, res) => {
+const addFood = async (req, res) => {
+  let image_filename = `${req.file.filename}`;
 
-}
+  const Food = new foodModel({
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+    category: req.body.category,
+    image: image_filename,
+  });
 
-export {addFood}
+  try {
+    await Food.save();
+    res.json({success: true, message: "Food item added successfully"});
+  } catch (error) {
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+export { addFood };
