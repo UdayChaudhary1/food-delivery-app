04a2e5f907cb396868d5019be8e75da1ad9b9eea
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-03-02 09:13:48 +0530

    Add Navbar and Sidebar component to app.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,15 @@
+import Navbar from "./components/Navbar/Navbar"
+import Sidebar from "./components/Sidebar/Sidebar"
+
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Navbar />
+      <hr />
+      <div className="app-content">
+        <Sidebar />
+      </div>
+    </div>
   )
 }
 export default App
