341db37802fc6765c64022c3f69e270769ed6307
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-03-03 09:33:41 +0530

    Use list usestate to store food list from backend

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -1,8 +1,21 @@
-import './List.css'
+import { useEffect, useState } from "react";
+import "./List.css";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const List = () => {
-  return (
-    <div>List</div>
-  )
-}
-export default List
+  const url = "http://localhost:4000";
+  const [list, setList] = useState([]);
+  const fetchList = async () => {
+    const response = await axios.get(`${url}/api/food/list`);
+    console.log(response.data);
+    if (response.data.success) {
+      setList(response.data.data);
+    } else {
+      toast.error("Error");
+    }
+  };
+  
+  return <div>List</div>;
+};
+export default List;
