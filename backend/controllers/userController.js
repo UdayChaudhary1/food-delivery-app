8fa37bf14ab558641f0a48fcdbfc3707d76e42b9
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-03-03 12:26:53 +0530

    Add loginUser and registerUser function

---
+++ backend/controllers/userController.js
@@ -0,0 +1,14 @@
+import UserModel from "../models/useModel";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import validator from "validator";
+
+// login user
+const loginUser = async (req, res) => {
+  
+}
+
+//register user
+const registerUser = async (req, res) => {
+  
+}
