08cadf55b24d8e773708c4eb83575ddf753b7f26
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-03-03 09:16:53 +0530

    Store form data in the backend

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import { assets } from "../../assets/assets";
 import "./Add.css";
+import axios from "axios";
 
 const Add = () => {
+  const url = "http://localhost:4000";
   const [image, setImage] = useState(false);
   const [data, setData] = useState({
     name: "",
@@ -17,9 +19,29 @@ const Add = () => {
     setData((data) => ({ ...data, [name]: value }));
   };
 
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("name", data.name);
+    formData.append("description", data.description);
+    formData.append("price", Number(data.price));
+    formData.append("category", data.category);
+    formData.append("image", image);
+    const response = await axios.post(`${url}/api/food/add`, formData);
+    if (response.data.success) {
+      setData({
+        name: "",
+        description: "",
+        price: "",
+        category: "Salad",
+      });
+      setImage(false);
+    }
+  };
+
   return (
     <div className="add">
-      <form className="flex-col">
+      <form className="flex-col" onSubmit={onSubmitHandler}>
         <div className="add-img-upload flex-col">
           <p>upload image</p>
           <label htmlFor="image">
