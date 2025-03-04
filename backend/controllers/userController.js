a2d2f7f6217343521be33be602c274970a475459
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-03-04 12:30:51 +0530

    Remove expiresIn

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -25,9 +25,7 @@ const loginUser = async (req, res) => {
 };
 
 const createToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: "1d",
-  });
+  return jwt.sign({ id }, process.env.JWT_SECRET);
 };
 
 //register user
