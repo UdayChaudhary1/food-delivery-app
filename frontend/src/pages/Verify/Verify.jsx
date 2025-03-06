0528608c121f4d156ada8db49341958af816970b
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-03-06 09:16:36 +0530

    Use searchParams to extract success and orderId

--- frontend/src/pages/Verify/Verify.jsx
+++ frontend/src/pages/Verify/Verify.jsx
@@ -1,8 +1,18 @@
-import './Verify.css'
+import { useSearchParams } from "react-router-dom";
+import "./Verify.css";
+import { useContext } from "react";
+import { StoreContext } from "../../context/StoreContext";
 
 const Verify = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const success = searchParams.get("success");
+  const orderId = searchParams.get("orderId");
+  const {url} = useContext(StoreContext);
+  
   return (
-    <div>Verify</div>
-  )
-}
-export default Verify
+    <div className="verify">
+      <div className="spinner"></div>
+    </div>
+  );
+};
+export default Verify;
