9152ceedc909e0da9b1e914df292b3a5b58cc21c
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-03-06 15:38:34 +0530

    Fetch all orders to display on frontend

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -1,8 +1,24 @@
-import './Orders.css'
+import { useEffect, useState } from "react";
+import "./Orders.css";
+import axios from "axios";
+import { toast } from "react-toastify";
 
-const Orders = () => {
-  return (
-    <div>Orders</div>
-  )
-}
-export default Orders
+const Orders = ({ url }) => {
+  const [orders, setOrders] = useState([]);
+
+  const fetchAllOrders = async () => {
+    const response = await axios.get(`${url}/api/order/list`);
+    if (response.data.success) {
+      setOrders(response.data.data);
+    } else {
+      toast.error("Error");
+    }
+  };
+
+  useEffect(() => {
+    fetchAllOrders();
+  }, []);
+
+  return <div>Orders</div>;
+};
+export default Orders;
