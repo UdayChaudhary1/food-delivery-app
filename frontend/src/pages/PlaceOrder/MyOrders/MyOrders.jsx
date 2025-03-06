4b07c311e8f244feba1ec23e7641007518fb2806
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-03-06 12:44:18 +0530

    Add logic to fetchOrders

--- frontend/src/pages/PlaceOrder/MyOrders/MyOrders.jsx
+++ frontend/src/pages/PlaceOrder/MyOrders/MyOrders.jsx
@@ -1,6 +1,30 @@
-import './MyOrders.css'
+import { useContext, useEffect, useState } from "react";
+import "./MyOrders.css";
+import { StoreContext } from "../../../context/StoreContext";
+import axios from "axios";
+import { assets } from "../../../assets/assets";
 
 const MyOrders = () => {
+  const { url, token } = useContext(StoreContext);
+  const [data, setData] = useState([]);
+
+  const fetchOrders = async () => {
+    const response = await axios.post(
+      `${url}/api/order/userorders`,
+      {},
+      {
+        headers: { token },
+      }
+    );
+    setData(response.data.data);
+  };
+
+  useEffect(() => {
+    if (token) {
+      fetchOrders();
+    }
+  }, [token]);
+
   return (
     <div>MyOrders</div>
   )
