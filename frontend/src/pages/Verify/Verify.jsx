743e03d4b09f42ad1af00cd5dbf1deec423d2da8
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-03-06 12:01:26 +0530

    Add logic to verify payment

--- frontend/src/pages/Verify/Verify.jsx
+++ frontend/src/pages/Verify/Verify.jsx
@@ -1,14 +1,33 @@
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import "./Verify.css";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { StoreContext } from "../../context/StoreContext";
+import axios from "axios";
 
 const Verify = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const success = searchParams.get("success");
   const orderId = searchParams.get("orderId");
-  const {url} = useContext(StoreContext);
-  
+  const { url } = useContext(StoreContext);
+  const navigate = useNavigate();
+
+  const verifyPayment = async () => {
+    const response = await axios.post(`${url}/api/order/verify`, {
+      orderId,
+      success,
+    });
+    console.log(response.data.success);
+    if (response.data.success) {
+      navigate("/myorders");
+    } else {
+      navigate("/");
+    }
+  };
+
+  useEffect(() => {
+    verifyPayment();
+  }, []);
+
   return (
     <div className="verify">
       <div className="spinner"></div>
