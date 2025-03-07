618db935dc17005cf6657e6776e280d870c556d0
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-03-07 12:24:23 +0530

    Add statusHandler

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -16,6 +16,18 @@ const Orders = ({ url }) => {
     }
   };
 
+  const statusHandler = async (event, orderId) => {
+    const response = await axios.post(`${url}/api/order/status`, {
+      orderId,
+      status: event.target.value,
+    });
+    if (response.data.success) {
+      await fetchAllOrders();
+    } else {
+      toast.error("Error");
+    }
+  };
+
   useEffect(() => {
     fetchAllOrders();
   }, []);
@@ -57,7 +69,7 @@ const Orders = ({ url }) => {
             </div>
             <p>Items: {order.items.length}</p>
             <p>Amount: ${order.amount}</p>
-            <select>
+            <select onChange={(e) => statusHandler(e, order._id)} value={order.status}>
               <option value="Food Processing">Food Processing</option>
               <option value="Out for delivery">Out for delivery</option>
               <option value="Delivered">Delivered</option>
