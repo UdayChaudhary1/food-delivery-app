8259bcfa3320e8e870673a57c66bcfa3519516c2
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2025-03-10 14:48:48 +0530

    Fix import userModel

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,4 @@
-import UserModel from "../models/UserModel.js";
+import UserModel from "../models/userModel";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import validator from "validator";
